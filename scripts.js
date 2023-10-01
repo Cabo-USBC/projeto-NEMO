d944596ec2cf07f41f2fe2a22734a4e8b5f7577e
Cabo-USBC <65255363+Cabo-USBC@users.noreply.github.com>
2023-10-01 18:38:12 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,86 @@
+//document.getElementById("imagem").innerHTML = "<p style='color: green;'> nome </p>"
+
+
+let min = 1;
+let max = 9;
+let numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(numeroAleatorio);
+//let numeroAleatorio = 7;
+//document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:px; left:px;'>"
+
+if(numeroAleatorio == 1){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:87px; left:25px;'>"
+
+  
+}
+else if(numeroAleatorio == 2){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:87px; left:88px;'>"
+  
+}
+else if(numeroAleatorio == 3){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:87px; left:151px;'>"
+  
+}
+else if(numeroAleatorio == 4){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:150px; left:25px;'>"
+  
+}
+else if(numeroAleatorio == 5){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:150px; left:88px;'>"
+  
+}
+else if(numeroAleatorio == 6){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:150px; left:151px;'>"
+  
+}
+else if(numeroAleatorio == 7){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:214px; left:25px;'>"
+  
+}
+else if(numeroAleatorio == 8){
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:214px; left:88px;'>"
+  
+}
+else{
+document.getElementById("imagem").innerHTML = "<img src=https://i.pinimg.com/474x/5a/ca/a1/5acaa1fe9cc5c7bfef54befd8de3e3a6.jpg height = '62';div style='position:absolute; top:214px; left:151px;'>"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
